f3217eefec920b0c9cbf491a84ab0397c171cd2c
KizMistry <Kiz_mistry@hotmail.com>
2023-03-17 17:27:38 +0000

    remove field from Meal model and remove any related instances

--- blog/models.py
+++ blog/models.py
@@ -42,7 +42,6 @@ class Meal(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
