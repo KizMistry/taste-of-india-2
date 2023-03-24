36b1231397fc5e52ecfe0dc96ac630a888be1450
KizMistry <Kiz_mistry@hotmail.com>
2023-03-24 16:31:40 +0000

    update date and time field in booking model and form

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -8,7 +8,19 @@ class ReviewForm(forms.ModelForm):
         fields = ('body',)
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('name', 'email', 'phone', 'date', 'time', 'tables', 'notes',)
+        widgets = {
+            'date': DateInput(),
+            # 'time': TimeInput(),
+        }
