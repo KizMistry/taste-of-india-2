d3360ca2bbe1d4c0fac4a327ff1723e3a1da66a3
KizMistry <Kiz_mistry@hotmail.com>
2023-04-02 17:47:38 +0000

    add modal to booking list html, and basic css, js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,3 +8,28 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+
+let deleteModal = document.getElementById("deleteModal");
+let deleteButton = document.getElementById("deleteButton");
+let closeModal = document.getElementsByClassName('close-modal');
+
+// Open the modal when how to play button clicked
+
+deleteButton.onclick = function () {
+    deleteModal.style.display = "block";
+};
+
+// Close the modal when 'x' button is clicked
+
+closeModal.onclick = function () {
+    deleteModal.style.display = "none";
+};
+
+// Modal closed when anywhere outside the modal is clicked
+
+window.onclick = function (event) {
+    if (event.target == deleteModal) {
+        deleteModal.style.display = "none";
+    }
+};
